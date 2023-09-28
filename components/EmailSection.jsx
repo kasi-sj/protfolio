dc351e89b0cf03d3261a6d0c4ea1d127a69d7d2e
kasi_sj <jkasinathan007@gmail.com>
2023-09-28 08:36:53 +0530

    third

--- components/EmailSection.jsx
+++ components/EmailSection.jsx
@@ -5,10 +5,10 @@ const EmailSection = () => {
   return (
     <section className='grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4'>
       <div>
-        <h5 className='text-xl font-bold text-white my-2'>Let's Connect</h5>
+        <h5 className='text-xl font-bold text-white my-2'>{"Let's Connect"}</h5>
         <p className='text-[#ADB&BE] mb-4 max-w-md'>{" "}
-            I'm currently looking for new oppertunities, my inbox is always open.
-            Whether you have a question or just want to say hi, I'll try my best to get back to you! 
+            {"I'm currently looking for new oppertunities, my inbox is always open."+
+            "Whether you have a question or just want to say hi, I'll try my best to get back to you! "}
         </p>
         <div className='socials flex flex-row gap-2'>
             <Link hef="linkedin.com" >
